fd3e0f5bf4b276e4d0ff40a75b44bd8b126c70f7
cbarlow6 <cbarlowcpa@gmail.com>
2020-01-31 20:47:26 -0500

    initial commit

---
+++ newsgroup_excel_files.R
@@ -0,0 +1,24 @@
+install.packages("foreach")
+
+library(readxl)
+library(stringr)
+library(foreach)
+library(tibble)
+
+folder <- "20news-bydate/20news-bydate-test/1excel_files"
+files <- list.files(path = folder, full.names = TRUE)
+
+#texts_df <- read_excel("20news-bydate-test/misc.forsale_texts.xlsx")
+
+newsgroup_df <-foreach(i = 1:length(files), .combine = rbind) %do% 
+  {value <- read_excel(files[i], col_names = TRUE, n_max = 100)
+  row.names<-value[ ,1])
+  #tibble(post = value$Post_no, text = value$Text)
+  }
+
+value[ , 1]
+
+rowname
+tibbl
+row.names[1]<- (value, value[1,1])
+
